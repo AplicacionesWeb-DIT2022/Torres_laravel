ce1fdfc683ef1748ae1595c0f7c421140e35bf44
jntorres2014 <jntorres2014@gmail.com>
2022-04-22 18:58:54 -0300

    agregando boton

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,11 +21,14 @@
         </style>
     </head>
     <body class="antialiased">
+        <div class="relative flex items-top justify-center  bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" > 
+            <a href="{{ url('/jugador/create') }}" class="text-sm text-gray-700 dark:text-gray-500 underline relative   ">Crear</a>
+        </div>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">login</a>
+                        <a href="{{ url('/jugador/create') }}" class="text-sm text-gray-700 dark:text-gray-500 underline relative">Crear</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
